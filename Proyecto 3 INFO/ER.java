a23f09d2bbdbb422fa77cf269454f6d811cb7797
Andres Tobar <andrewtobar05@gmail.com>
2024-05-11 15:38:36 -0600

    Implementacion de main

--- Proyecto 3 INFO/ER.java
+++ Proyecto 3 INFO/ER.java
@@ -9,6 +9,11 @@ public class ER{
   private static int contRegExp;
   private static HashMap<Integer, int[]> firstPos;
 
+  public ER(String er){
+    regExp = er;
+
+  }
+
   public static String ExtendRE(String re){
     //ER -> ER# 
     StringBuilder extendedRE = new StringBuilder();
@@ -44,26 +49,10 @@ public class ER{
     return firstPos;
   }
 
-  public static void main(String args[]) throws Exception{
-    BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("write: ");
-    String prueba = teclado.readLine();
-    System.out.println(ExtendRE(prueba));
-    regNumerator(ExtendRE(prueba));
-  }
 
-  public static void toStringList(int[] list){
-    System.out.print("[ ");
-    for(int k : list){
-      if(k == list.length- 1){
-        System.out.print(String.valueOf(k));
-      } else {
-        System.out.print(String.valueOf(k)+",");
-      }
-      
-    }
-    System.out.print(" ]");
-  }
+
+
+
 
   public void toGLD(char[] alfabeto, int[][] afdTransiciones, int cantEstados, int[] estadosFinales, int estadoInicial){
     int estadoError = 0;
@@ -121,5 +110,64 @@ public class ER{
       e.printStackTrace();
     }
   }
-  
+
+  public static void main(String args[]) throws Exception{
+    BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("write: ");
+    String prueba = teclado.readLine();
+    System.out.println(ExtendRE(prueba));
+    regNumerator(ExtendRE(prueba));
+
+    
+    char[] alfabeto = {'a', 'b', 'c'};
+    System.out.println("ALFABETO: " + Arrays.toString(alfabeto));
+    System.out.println("Ingrese la expresion regular: ");
+    String re = teclado.readLine();
+    System.out.println("Desea convertir a: ");
+    System.out.println("1. AFD");
+    System.out.println("2. GLD");
+    System.out.println("3. Ambos");
+    int opcion = Integer.parseInt(teclado.readLine());
+    ER er = new ER(re);
+    switch (opcion) {
+      case 1:
+        // Implementar conversion a AFD
+        break;
+        /*
+      case 2:
+        int[][] afdTransiciones = getAFDTransiciones(alfabeto, re);
+        int cantEstados = getCantEstados(afdTransiciones);
+        int[] estadosFinales = getEstadosFinales(afdTransiciones);
+        int estadoInicial = 1;
+        toGLD(alfabeto, afdTransiciones, cantEstados, estadosFinales, estadoInicial);
+        // Implementar conversion a GLD
+        break;
+      case 3:
+        // Implementar conversion a AFD
+        // Implementar conversion a GLD
+        int[][] afdTransiciones = getAFDTransiciones(alfabeto, re);
+        int cantEstados = getCantEstados(afdTransiciones);
+        int[] estadosFinales = getEstadosFinales(afdTransiciones);
+        int estadoInicial = 1;
+        toGLD(alfabeto, afdTransiciones, cantEstados, estadosFinales, estadoInicial);
+        break;
+      default:
+        System.out.println("Opcion no valida");
+        break;
+      */
+    }
+  }
+
+  public static void toStringList(int[] list){
+    System.out.print("[ ");
+    for(int k : list){
+      if(k == list.length- 1){
+        System.out.print(String.valueOf(k));
+      } else {
+        System.out.print(String.valueOf(k)+",");
+      }
+      
+    }
+    System.out.print(" ]");
+  }
 }
