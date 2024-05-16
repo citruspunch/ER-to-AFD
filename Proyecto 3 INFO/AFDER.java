480c6a2c410eb82eae9cdaee1101ae0a28393f31
SebastianRivasUG <157664620+SebastianRivasUG@users.noreply.github.com>
2024-05-15 23:21:07 -0600

    metodo firstPos y followPos necesita correcciones

--- Proyecto 3 INFO/AFDER.java
+++ Proyecto 3 INFO/AFDER.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Node{
   protected String regex;
   protected Node operador, derivado1, derivado2;
-  protected HashSet<Integer> firstPos, lastPos, followPos;
+  protected HashSet<Integer> firstPos, lastPos;
   protected boolean kleene;
   protected int num;
 
@@ -17,7 +17,6 @@ class Node{
 
     firstPos = new HashSet<Integer>();
     lastPos = new HashSet<Integer>();
-    followPos = new HashSet<Integer>();
 
     this.kleene = false;
   }
@@ -90,21 +89,19 @@ class Node{
   public void addLast(int num){
     this.lastPos.add(num);
   }
-
-  public HashSet<Integer> getfollowPos(){
-    return this.followPos;
-  }
-
-  public void addFP(int num){
-    this.followPos.add(num);
-  }
-
-
 }
 
 public class AFDER{
   private static char[] operadores = {'|','*','.','+'};
   private static char[] alphabet = {'a','b','c','d','#'};
+
+  private static int[] regExpNum;
+  private static int contRegExp;
+
+  private static HashSet<Integer> firstPos = new HashSet<Integer>();
+  private static HashSet<Integer> lastPos = new HashSet<Integer>();
+  private static HashMap<Integer, HashSet<Integer>> followPos = new HashMap<Integer, HashSet<Integer>>();
+
 
   public static boolean inAlfabeto(char r){
     for(int l = 0; l<alphabet.length; l++){
@@ -119,7 +116,6 @@ public class AFDER{
     //ER -> ER# 
     StringBuilder extendedRE = new StringBuilder();
     boolean foundParentesis = false;
-    StringBuilder positivaParentesis = new StringBuilder();
     StringBuilder aux = new StringBuilder();
     int cont = 0;
     ArrayList<Integer> start = new ArrayList<Integer>();
@@ -129,7 +125,6 @@ public class AFDER{
     for(int i = 0; i<re.length(); i++){
       
       char reActual = re.charAt(i);
-      char reAux = re.charAt(i);
       char reNext = (i+1<re.length()) ? re.charAt(i+1):'\0';
       char reAnt = (i-1>= 0) ? re.charAt(i-1):'\0';
       
@@ -229,9 +224,12 @@ public class AFDER{
   }*/
 
   public static Node regex(String expresion, int num){
-    Node regex = new Node(expresion,num);
-    regex.addFirst(regex.getNum());
-    return regex;
+    Node regex0 = new Node(expresion,num);
+    regex0.addFirst(regex0.getNum());
+    regex0.addLast(regex0.getNum());
+    System.out.println(regex0.getRegex()+regex0.getfirstPos());
+    System.out.println(regex0.getRegex()+regex0.getlastPos());
+    return regex0;
   }
 
   public static int jerarquia(char op){
@@ -252,8 +250,8 @@ public class AFDER{
 
   public static Node sintaxis(String regex){
     regex = extendRE(regex);
-    ArrayList<Character> operators = new ArrayList<>();
-    ArrayList<Node> silabas = new ArrayList<>();
+    ArrayList<Character> operators = new ArrayList<Character>();
+    ArrayList<Node> silabas = new ArrayList<Node>();
     int posicion = 0;
 
     for(int i = 0; i<regex.length(); i++){
@@ -324,13 +322,9 @@ public class AFDER{
       }
       operators.remove(operators.size()-1);
     }
-    
-    /*System.out.println("silabas:");
-    for (int i = 0; i < silabas.size(); i++) {
-        System.out.println("[" + i + "]: " + silabas.get(i).getRegex());
-    }*/
-    Node syntax = silabas.get(0);
-    return syntax;
+
+    Node sintax = silabas.get(0);
+    return sintax;
   }
 
   public static void printN(Node nodo, int capa){
@@ -359,18 +353,93 @@ public class AFDER{
     }
   }
 
-  public static void followPosition(Node arbol){
-    
+  public static void toStringList(int[] list){
+    System.out.print("[ ");
+    for(int k : list){
+      if(k == list.length ){
+        System.out.print(String.valueOf(k));
+      } else {
+        System.out.print(String.valueOf(k)+",");
+      }
+
+    }
+    System.out.print(" ]");
   }
+
+  public static int[] regNumerator(String re){
+    //[0,1,..,n] posiciones de ER
+    contRegExp = 0;
+    for(char i : re.toCharArray()){
+      if(!(i == '*' || i == '+' || i == '|' || i == '(' || i == ')' || i == '.' )){
+        contRegExp += 1;
+      }
+    }
+
+    regExpNum = new int[contRegExp];
+
+    for(int m = 0; m < contRegExp; m++){
+      regExpNum[m] = m + 1;
+      followPos.put(m+1, new HashSet<Integer>());
+    }
+
+    toStringList(regExpNum);
+
+    return regExpNum;
+  }
+
+  public static void fPlP(ArrayList<Node> nodos){
+    /*[. -> [#], [. -> [b], [. -> [b], [. -> [a], [* -> [| -> [b], [a]]]]]]] Recorrerlo al reves de hoja a root*/
+  }
+  
+  public static String toHashString(HashSet<Integer> s){
+    StringBuilder hash = new StringBuilder();
+    hash.append("[ ");
+      for(int i : s){
+        hash.append(i).append(", ");
+      }
+    hash.append(" ]");
+    return hash.toString();
+  }
+
+  public static String nodesToString(Node nodo) {
+    StringBuilder array = new StringBuilder();
+    array.append("[");
+    array.append(nodo.getRegex());
+    if (nodo.getDerivado1() != null || nodo.getDerivado2() != null) {
+        array.append(" -> ");
+        if (nodo.getDerivado1() != null) {
+            array.append(nodesToString(nodo.getDerivado1()));
+        }
+        if (nodo.getDerivado2() != null) {
+            if (nodo.getDerivado1() != null) {
+                array.append(", ");
+            }
+            array.append(nodesToString(nodo.getDerivado2()));
+        }
+    }
+    array.append("]");
+    return array.toString();
+  }
+
+  /*public static HashMap<Integer,HashSet<Integer>> followPosition(Node arbol){
+    /*if(arbol.getRegex()){
+
+    }
+    return followPos;
+  }*/
   
   public static void main(String args[]) throws Exception{
     BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("write: ");
     String prueba = teclado.readLine();
     System.out.println(extendRE(prueba));
+    regNumerator(extendRE(prueba));
     System.out.print("\n");
     Node test = sintaxis(prueba);
-    printN(test,0);   
+    printN(test,0); 
+    System.out.println("\n");
+    System.out.println(nodesToString(test));
+    //fPlP(sintaxis(prueba), firstPos, lastPos);
   }
 
 }
